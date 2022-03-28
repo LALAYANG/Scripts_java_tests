9079281da94f051b2e2ee79fb7cb157c22ba27e5
Yang Chen <56383562+LALAYANG@users.noreply.github.com>
2022-03-29 01:32:41 +0800

    Update get_modules.py

--- final_scripts/get_modules.py
+++ final_scripts/get_modules.py
@@ -1,18 +1,37 @@
-# get all project-sha-module turples
+# get all od tests and project-sha-module turples where the od tests are in
 # usage: python get_modules.py
 # input: idoft/pr-data.csv
-# output: projects_module.csv
+# output: all_od_tests.csv projects_modules.csv
 
 import csv
 
 idoft_pr_data_path = '../pr-data.csv'
 
-output_projects_csv = '../projects_module.csv'
+output_od_tests_csv = '../all_od_tests.csv'
+output_projects_csv = '../projects_modules.csv'
 
+od_tests_info = []
 project_module_info = []
 
-def get_projects_to_clone():
+
+def get_all_od_tests():
     pr_data = open(idoft_pr_data_path,"r")
+    pr_data_reader = csv.reader(pr_data)
+
+    for eachline in pr_data_reader:
+        if 'OD' in eachline[4] and 'NOD' not in eachline[4] and 'NDOD' not in eachline[4]:
+            if eachline[0:6] not in od_tests_info:
+                od_tests_info.append(eachline[0:5])
+    
+    with open(output_od_tests_csv,"w",newline='') as od_csv:
+        writer = csv.writer(od_csv)
+        writer.writerow(['Project URL','sha','module','test','category'])
+        for each in od_tests_info:
+            writer.writerow(each)
+
+
+def get_projects_to_clone():
+    pr_data = open(output_od_tests_csv,"r")
     pr_data_reader = csv.reader(pr_data)
 
     for eachline in pr_data_reader:
@@ -26,4 +45,6 @@ def get_projects_to_clone():
         for each in project_module_info:
             writer.writerow(each)
 
+
+get_all_od_tests()
 get_projects_to_clone()
