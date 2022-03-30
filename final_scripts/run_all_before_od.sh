a41c0e10b94bad397d0bad1e3b3bbd7611db1954
LALAYANG <583334298@qq.com>
2022-03-30 08:45:09 -0700

    update

--- final_scripts/run_all_before_od.sh
+++ final_scripts/run_all_before_od.sh
@@ -9,5 +9,5 @@ mkdir -p $(pwd)/xml_test_log
 echo $(pwd)
 cd ./ifixflakies_run_idoft/projects
 cd $project
-mvn -pl $module test -Dsurefire.runOrder=testorder -Dtest=$test_one,$test_two | tee -a $md5_str.log
+mvn -pl $module test -Dsurefire.runOrder=testorder -Dtest=$test_one,$test_two | tee ../../../xml_test_log/ter$md5.log
 cd ..
